d6faafd700eb711aa50333db72b12e66d2645ebe
Ivan Glushenkov <glushman966@gmail.com>
2026-09-08 15:58:04 +0300

    Защита от decompression bomb

--- users/utils.py
+++ users/utils.py
@@ -3,6 +3,10 @@ from PIL import Image, ImageOps
 
 AVATAR_SIZE = (300, 300)
 
+# Защита от decompression bomb: отклоняем изображения больше ~20 Мпикс.,
+# чтобы огромные картинки не «съедали» память при декодировании.
+Image.MAX_IMAGE_PIXELS = 20_000_000
+
 
 def resize_avatar(image):
     """Подгоняет изображение под размер аватара."""
