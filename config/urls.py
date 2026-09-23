bbda2b2323737aa546a5348a562ce0e9deafdf9e
Den-is-Ka <d_k81@mail.ru>
2026-09-23 21:33:49 +0500

    закрыл прямую раздачу chat файлов через django

--- config/urls.py
+++ config/urls.py
@@ -2,11 +2,17 @@ from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import include, path
 
+from chat.attachments import ProtectedChatAttachmentView
 from config import settings
 from config.views import home
 
 urlpatterns = [
     path("admin/", admin.site.urls),
+    path(
+        "media/chat_files/<path:attachment_path>",
+        ProtectedChatAttachmentView.as_view(),
+        name="protected_chat_attachment",
+    ),
     path("chat/", include("chat.urls")),
     path("users/", include("users.urls")),
     path("", home, name="home"),
